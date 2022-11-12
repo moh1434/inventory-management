a942bfc674e4d97121dc362913545d9c3f0b7aae
moh1434 <moh1434.ma@gmail.com>
2022-11-12 22:17:17 +0300

    fix navigateTo require a server

--- middleware/guest-only.ts
+++ middleware/guest-only.ts
@@ -1,10 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const user = useAuthUser()
+  const user = useAuthUser();
 
   if (user.value) {
-    if (process.server)
-      return navigateTo({ name: 'index' })
-
-    return abortNavigation()
+    return navigateTo({ name: "index" });
   }
-})
+});
