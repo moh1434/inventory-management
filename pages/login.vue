ea43d2241b41b2769f94d582758599f1c96dd9eb
moh1434 <moh1434.ma@gmail.com>
2022-11-14 12:51:05 +0300

    add temporary image

--- pages/login.vue
+++ pages/login.vue
@@ -14,8 +14,18 @@ async function onLoginSuccess() {
 
 <template>
   <div>
+    <img src="imgs/logo_background.png" class="img">
     <FormLogin @success="onLoginSuccess" />
     <div mb-3 flex gap-3>
     </div>
   </div>
 </template>
+
+<style>
+.img {
+  display: block;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
